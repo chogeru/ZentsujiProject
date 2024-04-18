05a5f2c72cebd88181795166596f93fc052652b6
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2024-04-18 22:24:02 +0900

    UI追加

---
+++ ZentsujiMetaverse/Assets/Hayashi/Script/OnlinePlayerSpawner.cs
@@ -0,0 +1,20 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlinePlayerSpawner : MonoBehaviour
+{
+    [SerializeField,Header("プレイヤースポーンプレハブ")]
+    private GameObject m_PlayerPrefabs;
+    [SerializeField,Header("スポーンポイント")]
+    private Transform m_PlyerSpownPoint;
+    void Start()
+    {
+        if (NetworkClient.isConnected)
+        {
+            // オンライン用プレイヤープレハブを生成
+            Instantiate(m_PlayerPrefabs,m_PlyerSpownPoint);
+        }
+    }
+}
